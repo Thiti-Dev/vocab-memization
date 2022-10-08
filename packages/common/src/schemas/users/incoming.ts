08d843eb19174cbfb62c9911acbf11af0da2a454
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-10-09 03:13:20 +0700

    chore: created  $ & $

---
+++ packages/common/src/schemas/users/incoming.ts
@@ -0,0 +1,52 @@
+import { Schema } from "jsonschema";
+
+export const $USER_REGISTER_SCHEMA$: Schema = {
+  id: "/User",
+  type: "object",
+  properties: {
+    username: {
+      type: "string",
+      required: true,
+      title: "username",
+      minLength: 5,
+      maxLength: 20,
+    },
+    password: {
+      type: "string",
+      required: true,
+      maxLength: 100,
+      minLength: 8,
+    },
+    first_name: {
+      type: "string",
+      required: true,
+      title: "FirstName",
+      minLength: 3,
+      maxLength: 25,
+    },
+    last_name: {
+      type: "string",
+      required: true,
+      title: "LastName",
+      minLength: 3,
+      maxLength: 40,
+    },
+  },
+};
+
+export const $USER_LOGIN_SCHEMA$: Schema = {
+  id: "/UserLogin",
+  type: "object",
+  properties: {
+    username: {
+      type: "string",
+      required: true,
+      title: "username",
+    },
+    password: {
+      type: "string",
+      required: true,
+      title: "password",
+    },
+  },
+};

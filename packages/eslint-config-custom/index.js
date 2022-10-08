0e68858265b3e194d78f8f07d37b432743ad62b6
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-10-09 03:09:26 +0700

    chore: set up the eslint config and rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "next/core-web-vitals"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "import/no-anonymous-default-export": "off",
   },
 };

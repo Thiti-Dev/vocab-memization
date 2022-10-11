1cd572a5f7e0025ff96b11a0ac225bb3007e3859
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-10-11 23:38:30 +0700

    added 2 common page -> Landing&NotFound page

---
+++ apps/vocab-memization-web/src/components/common/NotFound.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import {push} from 'svelte-spa-router'
+</script>
+
+<br />
+<br />
+<hr/>
+<strong>Page Not found</strong>
+<button on:click={() => push('/')}>Go to landing page</button>
